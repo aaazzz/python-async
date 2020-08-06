3622c47e14a6d833bc684a9734c42de58a40f8e7
Matsui Akira <a.matsui0725@gmail.com>
2020-08-06 15:22:14 +0700

    init

---
+++ main.py
@@ -0,0 +1,77 @@
+import time
+import asyncio
+import datetime
+import requests
+import json
+
+def calc_time(fn):
+    """関数の実行時間を計測するデコレータ"""
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        fn(*args, **kwargs)
+        end = time.time()
+        print(f"[{fn.__name__}] elapsed time: {end - start}")
+        return
+    return wrapper
+
+def make_request(url, headers):
+    return requests.get(url=url, headers=headers)
+
+async def get_request (url):
+    headers = {'Accept': 'application/json'}
+    loop = asyncio.get_event_loop()
+    response = await loop.run_in_executor(None, make_request, url, headers)
+    print(response.json()['joke'])
+
+
+@calc_time
+def main_async():
+    # Schedule the first call to get_joke()
+    loop = asyncio.get_event_loop()
+    tasks = asyncio.gather(
+            get_request('https://icanhazdadjoke.com/'),
+            get_request('https://icanhazdadjoke.com/'),
+            get_request('https://icanhazdadjoke.com/'),
+            get_request('https://icanhazdadjoke.com/'),
+            get_request('https://icanhazdadjoke.com/'),
+            get_request('https://icanhazdadjoke.com/'),
+            get_request('https://icanhazdadjoke.com/'),
+            get_request('https://icanhazdadjoke.com/'),
+            get_request('https://icanhazdadjoke.com/'),
+            get_request('https://icanhazdadjoke.com/'),
+            get_request('https://icanhazdadjoke.com/'),
+            get_request('https://icanhazdadjoke.com/'),
+            get_request('https://icanhazdadjoke.com/'),
+            get_request('https://icanhazdadjoke.com/'),
+            get_request('https://icanhazdadjoke.com/'),
+            get_request('https://icanhazdadjoke.com/'),
+            get_request('https://icanhazdadjoke.com/'),
+            get_request('https://icanhazdadjoke.com/'),
+            get_request('https://icanhazdadjoke.com/'),
+            get_request('https://icanhazdadjoke.com/'),
+            get_request('https://icanhazdadjoke.com/'),
+            get_request('https://icanhazdadjoke.com/'),
+            get_request('https://icanhazdadjoke.com/'),
+            get_request('https://icanhazdadjoke.com/'),
+            get_request('https://icanhazdadjoke.com/'),
+            get_request('https://icanhazdadjoke.com/'),
+            get_request('https://icanhazdadjoke.com/'),
+            get_request('https://icanhazdadjoke.com/'),
+            get_request('https://icanhazdadjoke.com/'),
+            get_request('https://icanhazdadjoke.com/'),
+            get_request('https://icanhazdadjoke.com/'),
+            get_request('https://icanhazdadjoke.com/'),
+            get_request('https://icanhazdadjoke.com/'),
+            get_request('https://icanhazdadjoke.com/'),
+            get_request('https://icanhazdadjoke.com/'),
+            get_request('https://icanhazdadjoke.com/'),
+            get_request('https://icanhazdadjoke.com/'),
+            get_request('https://icanhazdadjoke.com/'),
+            get_request('https://icanhazdadjoke.com/'),
+            get_request('https://icanhazdadjoke.com/'),
+        )
+    results = loop.run_until_complete(tasks)
+    return results
+
+if __name__ == "__main__":
+    main_async()
